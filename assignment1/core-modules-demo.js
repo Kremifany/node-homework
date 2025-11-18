116e66b07be06a599d86e97b7a8154f6f0481ccd
Kremifany <34751960+Kremifany@users.noreply.github.com>
2025-11-18 18:11:31 -0500

    Assignment1 after testing

--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -1,6 +1,7 @@
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
+const fsPromises = require('fs/promises')
 
 const sampleFilesDir = path.join(__dirname, 'sample-files');
 if (!fs.existsSync(sampleFilesDir)) {
@@ -8,11 +9,52 @@ if (!fs.existsSync(sampleFilesDir)) {
 }
 
 // OS module
+console.log(`Platform: ,${os.platform()}`);
+console.log(`CPU: ,${os.cpus()[0].model}`)
+console.log(`Total Memory: ${os.totalmem}`)
 
 
 // Path module
+const filePath = path.join(__dirname,"./sample-files/demo.txt");
+console.log(`Joined path: ${filePath}`)
 
 // fs.promises API
 
 
+const writeReadFile = async () =>{
+ try {
+      await fsPromises.writeFile(filePath, "Hello from fs.promises!", 'utf8');
+      const fileReadHandle = await fsPromises.readFile(filePath,'utf-8');
+      console.log("fs.promises read: ", fileReadHandle) 
+    } catch (err) {
+       console.log("An error occurred in reading data.", err);
+       return
+    }
+  }
+
+  writeReadFile()
 // Streams for large files- log first 40 chars of each chunk
+
+const filePath2 = path.join(__dirname,"./sample-files/largefile.txt");
+const writeReadLargeFile = async () =>{
+  try {
+      await fsPromises.writeFile(filePath2, " ", 'utf8');
+      for(let i=0; i<100; i++){
+        await fsPromises.appendFile(filePath2,"This is a line in a large file...\n");
+      }
+      const reader =fs.createReadStream(filePath2,{ encoding: 'utf8',highWaterMark: 1024 })
+      reader.on('data', (chunk) => {
+        const preview = chunk.slice(0, 40);
+        console.log("Read chunk: ", chunk)
+      });
+      reader.on('end',() => {
+        console.log("Finished reading large file with streams.")
+      })
+
+    } catch (err) {
+       console.log("An error occurred in reading data.", err);
+       return
+    }
+  }
+
+writeReadLargeFile()

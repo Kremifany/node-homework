693c52e21b9aaefd0458f73be4bb08b6a177bfac
Kremifany <34751960+Kremifany@users.noreply.github.com>
2026-01-07 11:00:45 -0500

    task search route added

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 const router = express.Router();
+
 const { userProductivity }  = require("../controllers/analyticsController");
 const { usersTaskStats } = require("../controllers/analyticsController");
+const { taskSearch } = require ("../controllers/analyticsController");
+
 
 router.route("/users").get(usersTaskStats);
 router.route("/users/:id").get(userProductivity);
+router.route("/tasks/search").get(taskSearch);
 
 module.exports = router;

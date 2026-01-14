341d909302d50144282de7f30da9da8052dd378f
Kremifany <34751960+Kremifany@users.noreply.github.com>
2026-01-14 10:17:03 -0500

    security with JWT and CSRF added into routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 const { register, logon, logoff, show } = require("../controllers/userController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
+
 
 router.route("/").post(register);
+
+router.use(jwtMiddleware);
 
 router.route("/logon").post(logon);
 
